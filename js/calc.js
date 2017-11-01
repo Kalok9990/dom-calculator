04fb3ac8e3a1663a1d0d329cc0fa85b0792b00da
Tech-A29 <Tech-A29@Tech-A29s-Air.lan>
2017-11-01 20:51:38 +0000

    zero added

--- js/calc.js
+++ js/calc.js
@@ -1,25 +1,34 @@
-var screen = document.getElementById("screen");
-var Nums =[];
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("The content is loaded");
 
-//Get values
-var buttons = document.querySelectorAll("button");
-for (var i = 0; i < buttons.length; i++) {
-  var button = buttons[i];
-  button.addEventListener("click", function(event){
-    screen.innerHTML = this.value;
-    Nums.push(this.value);
-  })
-}
+  var screen = document.getElementById("screen");
+  var Nums =[];
 
-//Equation
+  //Get values
+  var buttons = document.querySelectorAll("button");
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click", function(event){
+      Nums.push(this.value);
+      var equ = Nums.join("")
+      screen.innerHTML = equ;
+    })
+  }
 
+  //Equation
+  var equal = document.querySelector(".equals");
+  equal.addEventListener("click", eqtn);
+  function eqtn(){
+    var str = Nums.join("");
+    var total =eval(str);
+    screen.innerHTML = total;
+  }
 
-//Press AC to clear
-var ac = document.querySelector(".buttonClear");
-debugger
-ac.addEventListener("click", clear);
-
-function clear(){
-  screen.innerHTML = "Sparta Calculator";
-  Nums = [];
-}
+  //Press AC to clear
+  var ac = document.querySelector(".buttonClear");
+  ac.addEventListener("click", clear);
+  function clear(){
+    screen.innerHTML = "Sparta Calculator";
+    Nums = [];
+  }
+});
